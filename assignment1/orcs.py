f75ba409c02a06d6136cd428efbbb5be5f8ba314
Conor Fox <conor@conorfox.com>
2020-10-19 18:12:33 +0100

    Start of assignment

---
+++ assignment1/orcs.py
@@ -0,0 +1,62 @@
+class Orc:
+    def __init__(self, orc_name, strength, weapon):
+        self._name = orc_name
+        self._strength = strength
+        self._weapon = weapon
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, orc_name):
+        if isinstance(orc_name, str):
+            self._name = orc_name
+        else:
+            print("type ERROR")
+
+    @property
+    def strength(self):
+        return self._strength
+
+    @strength.setter
+    def strength(self, strength):
+        if isinstance(strength, (float, int)):
+            if strength < 0:
+                self._strength = 0.0
+            elif strength > 5:
+                self._strength = 5.0
+            else:
+                self._strength = float(strength)
+        else:
+            print("type ERROR")
+
+    @property
+    def weapon(self):
+        return self._weapon
+
+    @weapon.setter
+    def weapon(self, weapon):
+        if isinstance(weapon, bool):
+            self._weapon = weapon
+        else:
+            print("type ERROR")
+
+    def fight_with(self, other_orc):
+        return
+
+    def __gt__(self, other_orc):
+        return
+
+    def __str__(self):
+        orc_string = "{}, {}, {}".format(self._name, self._strength, self._weapon)
+        return orc_string
+
+def main():
+    my_orc = Orc('Conor', 5, True)
+    my_orc.name = 'Bob'
+    my_orc.strength = 3
+    print(my_orc)
+
+if __name__ == '__main__':
+    main()
