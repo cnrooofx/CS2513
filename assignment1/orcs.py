6e20bba3b8128809d792f365c9dce2c18f08eb42
Conor Fox <conor@conorfox.com>
2020-11-01 12:33:46 +0000

    Add docstrings

--- assignment1/orcs.py
+++ assignment1/orcs.py
@@ -1,5 +1,27 @@
+"""CS2513 Assignment 1 Submission.
+
+Script Name: orcs.py
+Author: Conor Fox 119322236
+"""
+
+
 class Orc:
+    """Class to make an orc character for a video game.
+
+    Attributes:
+        _name: String for the name of the orc.
+        _strength: Float between 0.0 and 5.0
+        _weapon: Boolean value to represent a weapon.
+    """
+
     def __init__(self, orc_name, strength, weapon):
+        """Construct an Orc object.
+
+        Args:
+            orc_name (str): The name for the Orc
+            strength (float): Value between 0 and 5 for the fighting strength
+            weapon (bool): True if the Orc has a weapon, False if not
+        """
         if isinstance(orc_name, str):
             self._name = orc_name
         else:
@@ -25,10 +47,12 @@ class Orc:
 
     @property
     def name(self):
+        """Return the name of the orc."""
         return self._name
 
     @name.setter
     def name(self, orc_name):
+        """Change the name of the orc."""
         if isinstance(orc_name, str):
             self._name = orc_name
         else:
@@ -36,10 +60,20 @@ class Orc:
 
     @property
     def strength(self):
+        """Return the strength value of the orc."""
         return self._strength
 
     @strength.setter
     def strength(self, strength):
+        """Change the strength value of the orc.
+
+        Notes:
+            If the value is less than 0 or greater than 5 it will be truncated
+            to the maximum or minimum value.
+
+        Args:
+            strength (float): Value between 0 and 5 for the Orc's strength
+        """
         if isinstance(strength, (float, int)):
             if strength < 0:
                 self._strength = 0.0
@@ -52,16 +86,33 @@ class Orc:
 
     @property
     def weapon(self):
+        """Return whether the orc has a weapon or not."""
         return self._weapon
 
     @weapon.setter
     def weapon(self, weapon):
+        """Change if the orc has a weapon.
+
+        Args:
+            weapon (bool): True if the Orc has a weapon, otherwise False
+        """
         if isinstance(weapon, bool):
             self._weapon = weapon
         else:
             print("type ERROR")
 
     def fight_with(self, other_orc):
+        """Fight with another orc object.
+
+        Notes:
+            If one orc has a weapon and the other does not, the one with the
+            weapon wins regardless of strength. If both orcs have weapons or
+            neither orc has a weapon, the orc with the highest strength wins.
+            If both orcs are equal in weapons and strength, both lose.
+
+        Args:
+            other_orc: Another Orc object to fight with.
+        """
         if isinstance(other_orc, Orc):
             if self > other_orc:
                 self.strength += 1
@@ -74,6 +125,7 @@ class Orc:
                 other_orc.strength -= 0.5
 
     def __gt__(self, other_orc):
+        """Return True if Orc is stronger than other_orc, otherwise False."""
         if self._weapon or other_orc.weapon:
             if self._weapon and other_orc.weapon:
                 if self._strength > other_orc.strength:
@@ -85,4 +137,5 @@ class Orc:
         return False
 
     def __str__(self):
+        """Return a string of the orc in the form: "name strength weapon"."""
         return "{} {} {}".format(self._name, self._strength, self._weapon)
