191bfc6cc0b6c43f43465915d865e5c6d7440679
Conor Fox <conor@conorfox.com>
2020-11-09 18:55:17 +0000

    More tests

--- assignment1/tests.py
+++ assignment1/tests.py
@@ -1,17 +1,17 @@
-from orcs import Orc
+from fox_conor import *
 
 print("----------------\nObject Creation\n----------------")
 
-my_orc = Orc(5, 5, True)
+my_orc = Orc('Orcy', 5, True)
 
 orc1 = Orc('Conor', 1.0, True)
 orc2 = Orc('Liam', 4.6, False)
-orc_error = Orc(3, 80, 'True')
+# orc_error = Orc(3, 80, 'True')
 
 print('My Orc:', my_orc)
 print('Orc 1:', orc1)
 print('Orc 2:', orc2)
-print('Orc w/ Error:', orc_error)
+# print('Orc w/ Error:', orc_error)
 
 print("----------------\nAttribute Changing\n----------------")
 
@@ -42,20 +42,20 @@ print("----------------\nFight With\n----------------")
 
 print('Before fighting', orc1, orc2)
 
-orc1.fight_with(orc2)
-orc2.fight_with(orc1)
+orc1.fight(orc2)
+orc2.fight(orc1)
 
 print('After fighting', orc1, orc2)
 
 orc2.weapon = True
 
-orc2.fight_with(orc1)
+orc2.fight(orc1)
 
 print('After fighting with weapons', orc1, orc2)
 
 orc1.strength = 5
 
-orc1.fight_with(orc2)
+orc1.fight(orc2)
 
 print('After fighting equal', orc1, orc2)
 
@@ -63,9 +63,9 @@ print("----------------\nTrying to Break\n----------------")
 
 hello = 'hello'
 
-orc1.fight_with(hello)
+orc1.fight(hello)
 
-print(orc2 < hello)
+# print(orc2 < hello)
 
 orc3 = Orc('Dylan', 2.6, True)
 orc4 = Orc('Conor', 2.5, True)
